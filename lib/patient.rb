c8d7d23ec38b352ab2ab06162daedc28e185f2b6
Luba Kassurova <kassurova@icloud.com>
2022-09-02 14:44:20 -0400

    commit tree 6f1f00ed64f8de4c3e4b21edbe932eda9931a486

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,27 @@
+class Patient
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+    def self.all
+      @@all
+    end
 
+    def appointments
+        Appointment.all.select do |app|
+            app.patient == self
+        end
+    end
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+      end
+      
+    def doctors
+        Appointment.all.map do |app|
+            app.doctor
+        end
+        
+      end
+end
